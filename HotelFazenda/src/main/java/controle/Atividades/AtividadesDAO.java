804c8df7d931e487213588b4be11331fa531ce5f
Andrei <andrei.m2005@aluno.ifsc.edu.br>
2024-03-07 08:06:41 -0300

    Atividades e quartos

--- HotelFazenda/src/main/java/controle/Atividades/AtividadesDAO.java
+++ HotelFazenda/src/main/java/controle/Atividades/AtividadesDAO.java
@@ -1,5 +1,6 @@
 package controle.Atividades;
 
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -9,129 +10,162 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 
+
+
 import controle.Conexao;
 import modelo.Atividades;
 import modelo.AtividadesHospedes;
-import modelo.Funcionarios;
+
 
 public class AtividadesDAO implements IAtividadesDAO
 
+
 {
-	private static AtividadesDAO instancia;
+    private static AtividadesDAO instancia;
 
-	public static AtividadesDAO getInstancia() {
-		if (instancia == null) {
-			instancia = new AtividadesDAO();
-		}
 
-		return instancia;
-	}
-	private AtividadesDAO() {}
-	@Override
-	public ArrayList<Atividades> ListarAtividades() {
-		
-		//ArrayList Que vai ser retornado
-		ArrayList<Atividades> atividades= new ArrayList<Atividades>();
-		
-		//Comando pro MySQL
-		String SQL = "SELECT * FROM Atividades";
-		
-		//Método pra fazer a conexão
-		Conexao con= Conexao.getConexao();
-		Connection conBD= con.conectar();
-		
-		try {
-			PreparedStatement Ps=conBD.prepareStatement(SQL);
-			
-			ResultSet Rs= Ps.executeQuery();
-			while (Rs.next()) {
-				Atividades At=new Atividades();
-				
-				int IdAtividade= Rs.getInt("id_atividade");
-				int RestricaoIdade= Rs.getInt("restricao_idade");
-				String Horario=Rs.getString("horario");
-				String HorarioFim=Rs.getString("horario_fim");
-				Funcionarios Funcionario=new Funcionarios();
-				String NomeAtividade=Rs.getString("nome_atividade");
-				Date Data=Rs.getDate("data");
-				
-				//Tem que preencher os atributos do objeto funcionário
-				At.setIdAtividade(IdAtividade);
-				At.setRestricaoIdade(RestricaoIdade);
-				At.setHorario(Horario);
-				At.setHorarioFim(HorarioFim);
-				At.setFuncionario(Funcionario);
-				At.setNomeAtividade(NomeAtividade);
-				At.setData(Data);
-				atividades.add(At);
-			}
-		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			con.fecharConexao();
-		}
-	
-		//Return da arraylist
-		return atividades;
-	}
+    public static AtividadesDAO getInstancia() {
+        if (instancia == null) {
+            instancia = new AtividadesDAO();
+        }
 
-	@Override
-	public boolean AtualizarAtividades(Atividades Ativ) {
-		// TODO Auto-generated method stub
-		
-		return false;
-	}
 
-	@Override
-	public boolean RemoverAtividades(Atividades Ativ) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+        return instancia;
+    }
+    private AtividadesDAO() {}
+    @Override
+    public ArrayList<Atividades> ListarAtividades() {
+        
+        //ArrayList Que vai ser retornado
+        ArrayList<Atividades> atividades= new ArrayList<Atividades>();
+        
+        //Comando pro MySQL
+        String SQL = "SELECT * FROM Atividades";
+        
+        //Método pra fazer a conexão
+        Conexao con= Conexao.getConexao();
+        Connection conBD= con.conectar();
+        
+        try {
+            PreparedStatement Ps=conBD.prepareStatement(SQL);
+            
+            ResultSet Rs= Ps.executeQuery();
+            while (Rs.next()) {
+                Atividades At=new Atividades();
+                
+                int IdAtividade= Rs.getInt("id_atividade");
+                int RestricaoIdade= Rs.getInt("restricao_idade");
+                String Horario=Rs.getString("horario");
+                String HorarioFim=Rs.getString("horario_fim");
+                int FuncionarioId=Rs.getInt("funcionario_id");
+                String NomeAtividade=Rs.getString("nome_atividade");
+                Date Data=Rs.getDate("data");
+                
+                At.setIdAtividade(IdAtividade);
+                At.setRestricaoIdade(RestricaoIdade);
+                At.setHorario(Horario);
+                At.setHorarioFim(HorarioFim);
+                At.setFuncionarioId(FuncionarioId);
+                At.setNomeAtividade(NomeAtividade);
+                At.setData(Data);
+                atividades.add(At);
+            }
+        } 
+        catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            con.fecharConexao();
+        }
+    
+        //Return da arraylist
+        return atividades;
+    }
 
-	@Override
-	public Atividades BuscarAtividadesPorLocal(String Local) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public int InserirAtividades(Atividades Ativ) {
-		// TODO Auto-generated method stub
-		String SQL= "INSERT INTO Atividades(horario,horario_fim,funcionario_id,restricao_idade,nome_atividade,data) VALUES (?,?,?,?,?,?)";
-		//Método pra fazer a conexão com o banco
-			Conexao con= Conexao.getConexao();
-			Connection conBD= con.conectar();
-			
-			
-		int ChavePrimariaGerada= Integer.MIN_VALUE;//variável que vai guardar a chave que vai ser gerada automaticamente, se não for autogerado não precisa disso
-		
-		
-			try {
-				PreparedStatement Ps= conBD.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);//se for um insert já conhcendo a chave primária não adcionar o Statement.RETURN_GENERATED_KEYS
-				Ps.setString(1, Ativ.getHorario());
-				Ps.setString(2, Ativ.getHorarioFim());
-				Ps.setInt(3, Ativ.getFuncionario().getFuncionarioId());
-				Ps.setInt(4, Ativ.getRestricaoIdade());
-				Ps.setString(5, Ativ.getNomeAtividade());
-				Ps.setDate(6, Ativ.getData());
-				
-				/*
-				 * se for um insert sem gerar chave primária automaticamente não usar a parte de baixo 
-				 */
-				ResultSet Rs= Ps.executeQuery();
-				if(Rs!=null) {
-					ChavePrimariaGerada= Rs.getInt(1);
-				}
-				
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			} finally {
-				con.fecharConexao();
-			}
-		
-		return ChavePrimariaGerada;
-	}
-	
+    @Override
+    public boolean AtualizarAtividades(Atividades Ativ) {
+
+
+            //Conexâo SQl a ser executada
+            String SQL = "UPDATE atividades SET NomeAtividade = ? WHERE IdAtividade = ?";
+            
+            //abre a conexão e cria a "parte de conexão" com MYSQL
+            Conexao con= Conexao.getInstancia();
+            Connection conBD= con.conectar();
+            
+            int retorno = 0;
+            try {
+                PreparedStatement ps = conBD.prepareStatement(SQL);
+                ps.setString(1, Ativ.getNomeAtividade());
+                ps.setInt(1, Ativ.getIdAtividade());
+                
+                retorno = ps.executeUpdate();
+                
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }finally{
+                
+            }
+        
+        return (retorno == 0 ? false :true);
+    }
+
+
+    @Override
+    public boolean RemoverAtividades(Atividades Ativ) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+
+    @Override
+    public Atividades BuscarAtividadesPorLocal(String Local) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public int InserirAtividades(Atividades Ativ) {
+        // TODO Auto-generated method stub
+        String SQL= "INSERT INTO Atividades(horario,horario_fim,funcionario_id,restricao_idade,nome_atividade,data) VALUES (?,?,?,?,?,?)";
+        //Método pra fazer a conexão com o banco
+            Conexao con= Conexao.getConexao();
+            Connection conBD= con.conectar();
+            
+            
+        int ChavePrimariaGerada= Integer.MIN_VALUE;//variável que vai guardar a chave que vai ser gerada automaticamente, se não for autogerado não precisa disso
+        
+        
+            try {
+                PreparedStatement Ps= conBD.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);//se for um insert já conhcendo a chave primária não adcionar o Statement.RETURN_GENERATED_KEYS
+                Ps.setString(1, Ativ.getHorario());
+                Ps.setString(2, Ativ.getHorarioFim());
+                Ps.setInt(3, Ativ.getFuncionarioId());
+                Ps.setInt(4, Ativ.getRestricaoIdade());
+                Ps.setString(5, Ativ.getNomeAtividade());
+                Ps.setDate(6, Ativ.getData());
+                
+                /*
+                 * se for um insert sem gerar chave primária automaticamente não usar a parte de baixo 
+                 */
+                ResultSet Rs= Ps.executeQuery();
+                if(Rs!=null) {
+                    ChavePrimariaGerada= Rs.getInt(1);
+                }
+                
+            } catch (SQLException e) {
+                
+                e.printStackTrace();
+            } finally {
+                con.fecharConexao();
+            }
+        
+        return ChavePrimariaGerada;
+    }
+    
 }
+
+
+
--- HotelFazenda/src/main/java/controle/Quartos/QuartosDAO.java
+++ HotelFazenda/src/main/java/controle/Quartos/QuartosDAO.java
@@ -1,139 +1,169 @@
 package controle.Quartos;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+
 import controle.Conexao;
 import controle.Funcionarios.FuncionariosDAO;
 import modelo.Funcionarios;
 import modelo.Quartos;
 public class QuartosDAO implements IQuartosDAO{
-	
+    
 private static QuartosDAO instancia;
-	
-	private QuartosDAO() {
-	} // construtor privado
+    
+    private QuartosDAO() {
+    } // construtor privado
 
-	/**
-	 * Metodo singleton
-	 * 
-	 * @return conexao
-	 */
 
-	public static QuartosDAO getConexao() {
-		if (instancia == null) {
-			instancia = new QuartosDAO();
-		}
+    /**
+     * Metodo singleton
+     * 
+     * @return conexao
+     */
 
-		return instancia;
-	}
-	
-	
-	@Override
-	public int inserirQuarto(Quartos end) {
-		// TODO Auto-generated method stub
-		
-		String SQL = "INSERT INTO Quartos (id_Quartos ,max_pessoas, manutencao, tipo_cama, frigobar, ar_condicionado, banheira, tv, preco_quarto_dia) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		
 
-		Conexao con = Conexao.getConexao();
-		Connection conBD = con.conectar();
-		
-		int ChavePrimariaGerada = Integer.MIN_VALUE;
-		
-		try {
-			PreparedStatement ps = conBD.prepareStatement(SQL);
-			ps.setInt(1, end.getIdQuartos());
-			ps.setInt(2, end.getMaxPessoas());
-			ps.setString(3, end.getManutencao());
-			ps.setString(4, end.getTipoCama());
-			ps.setBoolean(5, end.getFrigobar());
-			ps.setBoolean(6, end.getArCondicionado());
-			ps.setBoolean(7, end.getBanheira());
-			ps.setBoolean(8, end.getTV());
-			ps.setFloat(9, end.getPrecoDiaria());
-		
-			ResultSet Rs = ps.executeQuery();
-			if(Rs!=null) {
-				ChavePrimariaGerada=Rs.getInt(1);
-			}
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			con.fecharConexao();
-		}
-		
-		return 0;
-	}
-	@Override
-	public ArrayList<Quartos> ListarQuartos() {
-		// TODO Auto-generated method stub
-		
+    public static QuartosDAO getConexao() {
+        if (instancia == null) {
+            instancia = new QuartosDAO();
+        }
+
+
+        return instancia;
+    }
+    
+    
+    @Override
+    public int inserirQuarto(Quartos end) {
+        // TODO Auto-generated method stub
+        
+        String SQL = "INSERT INTO Quartos (id_Quartos ,max_pessoas, manutencao, tipo_cama, frigobar, ar_condicionado, banheira, tv, preco_quarto_dia) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+
+
+        Conexao con = Conexao.getConexao();
+        Connection conBD = con.conectar();
+        
+        int ChavePrimariaGerada = Integer.MIN_VALUE;
+        
+        try {
+            PreparedStatement ps = conBD.prepareStatement(SQL);
+            ps.setInt(1, end.getIdQuartos());
+            ps.setInt(2, end.getMaxPessoas());
+            ps.setString(3, end.getManutencao());
+            ps.setString(4, end.getTipoCama());
+            ps.setBoolean(5, end.getFrigobar());
+            ps.setBoolean(6, end.getArCondicionado());
+            ps.setBoolean(7, end.getBanheira());
+            ps.setBoolean(8, end.getTV());
+            ps.setFloat(9, end.getPrecoDiaria());
+        
+            ResultSet Rs = ps.executeQuery();
+            if(Rs!=null) {
+                ChavePrimariaGerada=Rs.getInt(1);
+            }
+            
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            con.fecharConexao();
+        }
+        
+        return 0;
+    }
+    @Override
+    public ArrayList<Quartos> ListarQuartos() {
+        // TODO Auto-generated method stub
+        
 ArrayList<Quartos> Funcionarios = new ArrayList<Quartos>();
-		
-		String SQL = "SELECT * FROM Quartos";
-		
-		Conexao con = Conexao.getConexao();
-		Connection conBD = con.conectar();
-		
-		try {
-			PreparedStatement ps = conBD.prepareStatement(SQL);
-			
-			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				
-				Quartos quartos = new Quartos();
-				
-				Integer id_Quartos = rs.getInt("id_Quartos");
-				Integer max_pessoas = rs.getInt("max_pessoas");
-				String manutencao = rs.getString("manutencao");
-				String tipo_cama = rs.getString("tipo_cama");
-				Boolean frigobar = rs.getBoolean("frigobar");
-				Boolean ar_condicionado = rs.getBoolean("ar_condiconado");
-				Boolean banheira = rs.getBoolean("banheira");
-				Boolean tv = rs.getBoolean("tv");
-				Float preco_quarto_dia= rs.getFloat("preco_quarto_dia");
-				
-				quartos.setIdQuartos(id_Quartos);
-				quartos.setMaxPessoas(max_pessoas);
-				quartos.setManutencao(manutencao);
-				quartos.setTipoCama(tipo_cama);
-				quartos.setFrigobar(frigobar);
-				quartos.setArCondicionado(ar_condicionado);
-				quartos.setBanheira(banheira);
-				quartos.setTV(tv);
-				quartos.setPrecoDiaria(preco_quarto_dia);
-				
-			}
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;
-		
-		
-	}
-	@Override
-	public boolean atualizarQuarto(Quartos end) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public boolean removerQuarto(Quartos end) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public Quartos buscarQuartoPorNumero(int cep) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        
+        String SQL = "SELECT * FROM Quartos";
+        
+        Conexao con = Conexao.getConexao();
+        Connection conBD = con.conectar();
+        
+        try {
+            PreparedStatement ps = conBD.prepareStatement(SQL);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()) {
+                
+                Quartos quartos = new Quartos();
+                
+                Integer id_Quartos = rs.getInt("id_Quartos");
+                Integer max_pessoas = rs.getInt("max_pessoas");
+                String manutencao = rs.getString("manutencao");
+                String tipo_cama = rs.getString("tipo_cama");
+                Boolean frigobar = rs.getBoolean("frigobar");
+                Boolean ar_condicionado = rs.getBoolean("ar_condiconado");
+                Boolean banheira = rs.getBoolean("banheira");
+                Boolean tv = rs.getBoolean("tv");
+                Float preco_quarto_dia= rs.getFloat("preco_quarto_dia");
+                
+                quartos.setIdQuartos(id_Quartos);
+                quartos.setMaxPessoas(max_pessoas);
+                quartos.setManutencao(manutencao);
+                quartos.setTipoCama(tipo_cama);
+                quartos.setFrigobar(frigobar);
+                quartos.setArCondicionado(ar_condicionado);
+                quartos.setBanheira(banheira);
+                quartos.setTV(tv);
+                quartos.setPrecoDiaria(preco_quarto_dia);
+                
+            }
+            
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return null;
+        
+        
+    }
+    @Override
+    public boolean atualizarQuarto(Quartos end) {
+
+
+        //Conexâo SQl a ser executada
+        String SQL = "UPDATE Quartos SET MaxPessoas = ? WHERE IdQuartos = ?";
+        
+        //abre a conexão e cria a "parte de conexão" com MYSQL
+        Conexao con= Conexao.getInstancia();
+        Connection conBD= con.conectar();
+        
+        int retorno = 0;
+        
+        try {
+            PreparedStatement ps = conBD.prepareStatement(SQL);
+            ps.setInt(1, end.getIdQuartos());
+            ps.setInt(1, end.getMaxPessoas());
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally {
+            
+        }
+
+
+        return (retorno == 0 ? false :true);
+    }
+    @Override
+    public boolean removerQuarto(Quartos end) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public Quartos buscarQuartoPorNumero(int cep) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
+
+
