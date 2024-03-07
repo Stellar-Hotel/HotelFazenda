295b32248e497098159bb34c8b8f8a464ae72401
Bernardo <bernardo.o2006@aluno.ifsc.edu.br>
2024-03-07 15:19:53 -0300

    Tela de cadastro quase pronta (Backup)

--- HotelFazenda/src/main/java/visao/TelaDeCadastro.java
+++ HotelFazenda/src/main/java/visao/TelaDeCadastro.java
@@ -15,6 +15,8 @@ import net.miginfocom.swing.MigLayout;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaDeCadastro extends JFrame {
 
@@ -24,7 +26,6 @@ public class TelaDeCadastro extends JFrame {
 	private JTextField textField_3;
 	private JTextField textField;
 	private JTextField textField_2;
-	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -34,7 +35,7 @@ public class TelaDeCadastro extends JFrame {
 			public void run() {
 				try {
 					TelaDeCadastro frame = new TelaDeCadastro();
-					frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+					//frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -54,72 +55,64 @@ public class TelaDeCadastro extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(
-				new MigLayout("", "[65px,grow][][4px][405px][93px][28px][33px][301px,grow][115px,grow][97px]", "[14px][14px][94px][25px][20px][27px][20px][:20px:30px,grow][20px,grow][grow][23px][][][grow][][][][][][][][][]"));
-
-		JLabel lblNewLabel = new JLabel("Bem-vindo ao");
-		contentPane.add(lblNewLabel, "flowx,cell 0 0,alignx left,aligny top");
-
-		JLabel lblNewLabel_7 = new JLabel("Já possui uma conta?");
-		contentPane.add(lblNewLabel_7, "cell 9 0,growx,aligny top");
-
-		JLabel lblNewLabel_8 = new JLabel("Entrar");
-		lblNewLabel_8.setForeground(new Color(117, 187, 68));
-		contentPane.add(lblNewLabel_8, "cell 9 1,alignx center,aligny top");
-
-		JLabel lblNewLabel_1 = new JLabel("Inscrever-se");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 55));
-		contentPane.add(lblNewLabel_1, "cell 0 2 4 1,alignx left,growy");
+				new MigLayout("", "[65px,grow][4px][405px][][93px][28px,grow][33px][115px,grow][97px]", "[14px][94px][20px][27px][20px][:20px:30px,grow][20px,grow][grow][23px][][][grow][]"));
+		
+		JLabel lblNewLabel = new JLabel("Bem-Vindo ao");
+		contentPane.add(lblNewLabel, "flowx,cell 0 0");
+						
+						JLabel lblNewLabel_7 = new JLabel("Já possui uma conta?");
+						contentPane.add(lblNewLabel_7, "cell 8 0");
+				
+						JLabel lblNewLabel_8 = new JLabel("Entrar");
+						lblNewLabel_8.setForeground(new Color(117, 187, 68));
+						contentPane.add(lblNewLabel_8, "cell 8 1,alignx center,aligny top");
+		
+				JLabel lblNewLabel_1 = new JLabel("Inscrever-se");
+				lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 55));
+				contentPane.add(lblNewLabel_1, "cell 0 2,alignx left,growy");
 
 		JLabel lblNewLabel_2 = new JLabel("Digite seu nome de usuário");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(lblNewLabel_2, "cell 0 6,growx,aligny top");
-				
-				JLabel lblNewLabel_2_1 = new JLabel("Digite seu nome de usuário");
-				lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-				contentPane.add(lblNewLabel_2_1, "cell 7 6");
-		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, "cell 0 7 4 1,grow");
-		panel.setLayout(new BorderLayout(0, 0));
-		
-		textField = new JTextField();
-		panel.add(textField);
-		textField.setColumns(10);
-		
-		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1, "cell 7 7,grow");
-		panel_1.setLayout(new BorderLayout(0, 0));
+		contentPane.add(lblNewLabel_2, "cell 0 4,growx,aligny top");
 		
 		textField_2 = new JTextField();
-		panel_1.add(textField_2, BorderLayout.CENTER);
+		contentPane.add(textField_2, "cell 0 5 9 1,growx");
 		textField_2.setColumns(10);
-		
-		JPanel panel_2 = new JPanel();
-		contentPane.add(panel_2, "cell 7 9,grow");
-		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		textField_4 = new JTextField();
-		panel_2.add(textField_4);
-		textField_4.setColumns(10);
-
-		JLabel lblNewLabel_3 = new JLabel("Digite seu nome");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(lblNewLabel_3, "cell 0 11,grow");
-		
-				textField_1 = new JTextField();
-				contentPane.add(textField_1, "cell 0 12 4 1,growx,aligny top");
-				textField_1.setColumns(10);
-
-		JLabel lblNewLabel_5 = new JLabel("Digite sua senha");
-		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		contentPane.add(lblNewLabel_5, "cell 0 17,growx,aligny top");
-
-		JLabel lblNewLabel_6 = new JLabel("Hotel Fazenda");
-		lblNewLabel_6.setForeground(new Color(117, 187, 68));
-		contentPane.add(lblNewLabel_6, "cell 0 0,alignx left,aligny top");
+						
+								JLabel lblNewLabel_3 = new JLabel("Digite seu nome");
+								lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+								contentPane.add(lblNewLabel_3, "cell 0 6,growx,aligny bottom");
 								
-										textField_3 = new JTextField();
-										contentPane.add(textField_3, "cell 0 18 4 1,growx,aligny top");
-										textField_3.setColumns(10);
+								JLabel lblNewLabel_4 = new JLabel("Telefone");
+								lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
+								contentPane.add(lblNewLabel_4, "cell 4 6,aligny bottom");
+						
+								textField_1 = new JTextField();
+								contentPane.add(textField_1, "cell 0 7 3 1,growx,aligny top");
+								textField_1.setColumns(10);
+														
+														textField = new JTextField();
+														contentPane.add(textField, "cell 4 7 5 1,growx,aligny top");
+														textField.setColumns(10);
+														
+																JLabel lblNewLabel_5 = new JLabel("Digite sua senha");
+																lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 20));
+																contentPane.add(lblNewLabel_5, "cell 0 8,growx,aligny top");
+														
+																textField_3 = new JTextField();
+																contentPane.add(textField_3, "cell 0 9 9 1,growx,aligny top");
+																textField_3.setColumns(10);
+																
+																JLabel lblNewLabel_6 = new JLabel("Hotel Fazenda");
+																lblNewLabel_6.setForeground(new Color(117, 187, 68));
+																contentPane.add(lblNewLabel_6, "cell 0 0");
+																
+																JButton btnNewButton = new JButton("New button");
+																btnNewButton.setBackground(new Color(117, 187, 68));
+																btnNewButton.addActionListener(new ActionListener() {
+																	public void actionPerformed(ActionEvent e) {
+																	}
+																});
+																contentPane.add(btnNewButton, "cell 3 11");
 	}
 }
