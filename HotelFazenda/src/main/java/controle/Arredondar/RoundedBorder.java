2580ba51c557f84c65f3029976d2c8b2c384a9df
andrei <andrei.m2005@aluno.ifsc.edu.br>
2024-03-28 14:17:58 -0300

    tela login arredondando borda

---
+++ HotelFazenda/src/main/java/controle/Arredondar/RoundedBorder.java
@@ -0,0 +1,35 @@
+package controle.Arredondar;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.border.AbstractBorder;
+
+public class RoundedBorder extends AbstractBorder {
+    private Color borderColor;
+    private int radius;
+
+    public RoundedBorder(Color borderColor, int radius) {
+        this.borderColor = borderColor;
+        this.radius = radius;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        super.paintBorder(c, g, x, y, width, height);
+        g.setColor(borderColor);
+        g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(radius, radius, radius, radius);
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c, Insets insets) {
+        insets.left = insets.top = insets.right = insets.bottom = radius;
+        return insets;
+    }
+}
