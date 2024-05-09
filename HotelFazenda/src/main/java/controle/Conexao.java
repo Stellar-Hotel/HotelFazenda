7cd9ea4f16070e6dcf39240c22850553dbe77507
GabrielFelipeMohr <gabriel.f2005@aluno.ifsc.edu.br>
2024-05-09 09:07:45 -0300

    Finalizado

--- HotelFazenda/src/main/java/controle/Conexao.java
+++ HotelFazenda/src/main/java/controle/Conexao.java
@@ -1,14 +1,18 @@
 package controle;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao { // Connection
 
-	private static final String USERNAME = "root";
-	private static final String SENHA = "aluno";
-	private static final String BD = "Stellar";
+	private static  String USERNAME;
+	private static  String SENHA ;
+	private static  String BD ;
 	private Connection con; // jdbc
 	private static Conexao instancia; // singleton
 
@@ -24,9 +28,39 @@ public class Conexao { // Connection
 	public static Conexao getConexao() {
 		if (instancia == null) {
 			instancia = new Conexao();
+			LerArquivoBD();
 		}
 
 		return instancia;
+	}
+	
+	public static void LerArquivoBD() {
+		FileReader arquivo;
+		try {
+			arquivo = new FileReader("credenciais.txt");
+			
+			if(arquivo!=null)
+			{
+				BufferedReader reader=new BufferedReader(arquivo);
+				
+				USERNAME=reader.readLine();
+				SENHA=reader.readLine();
+				BD=reader.readLine();
+				
+				reader.close();
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 	}
 
 	/**
