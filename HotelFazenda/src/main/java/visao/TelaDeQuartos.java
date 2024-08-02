0958df9d68dc3e0786df7a9a53a7407fde64d53c
erik <erik.r01@aluno.ifsc.edu.br>
2024-08-02 10:54:17 -0300

    arrumado

--- HotelFazenda/src/main/java/visao/TelaDeQuartos.java
+++ HotelFazenda/src/main/java/visao/TelaDeQuartos.java
@@ -14,12 +14,14 @@ import controle.Hospedagens.HospedagensDAO;
 import controle.Hospede.HospedeDAO;
 import controle.Quartos.QuartosDAO;
 import modelo.Atividades;
+import modelo.CurrentFunc;
 import modelo.Funcionarios;
 import modelo.Hospedagens;
 import modelo.Hospedes;
 import modelo.Quartos;
 import modelo.Servicos;
 import net.miginfocom.swing.MigLayout;
+import utils.DefaultScreen;
 
 import java.awt.GridLayout;
 import javax.swing.JLabel;
@@ -55,7 +57,7 @@ import javax.swing.border.LineBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-public class TelaDeQuartos extends JFrame {
+public class TelaDeQuartos extends DefaultScreen {
 
 	private ArrayList<Hospedagens> ListaHospedagens;
 	private ArrayList<Quartos> ListaQuartos;
@@ -118,7 +120,9 @@ public class TelaDeQuartos extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public TelaDeQuartos(int x, Funcionarios Func) {
+	public TelaDeQuartos(int x ) {
+		super();
+		Funcionarios Func = CurrentFunc.getInstance().getLoggedInFuncionario();
 		setTitle("Tela de Quartos");
 
 		Quartos quartos = new Quartos();
@@ -150,202 +154,7 @@ public class TelaDeQuartos extends JFrame {
 			e.printStackTrace();
 		}
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1354, 879);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[200px:200px:200px][830.00,grow]",
-				"[40px:49.00px:40px][571.00,grow,fill][60px:60px:60px]"));
-
-		JPanel BarraLateral = new JPanel();
-		BarraLateral.setBackground(new Color(255, 255, 255));
-		contentPane.add(BarraLateral, "cell 0 1 1 2,grow");
-		BarraLateral.setLayout(new MigLayout("", "[:200:200,grow]",
-				"[20px:20px:20px][40px][40px][40px][40px][40px][40px][40px][211.00,grow][98.00]"));
-
-		JLabel lblHome = new JLabel("Home");
-		lblHome.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Home TelaHome = new Home( );
-				TelaHome.setVisible(true);
-				TelaHome.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-				dispose();
-			}
-		});
-		lblHome.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-		lblHome.setBackground(new Color(0, 204, 0));
-		lblHome.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/Home.jpg")));
-		BarraLateral.add(lblHome, "cell 0 1,grow");
-
-		JLabel lblHospede = new JLabel("Hospede");
-		lblHospede.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				TelaDeHospedes Chama = new TelaDeHospedes( );
-				Chama.setVisible(true);
-				Chama.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-				dispose();
-
-			}
-		});
-		lblHospede.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-		lblHospede.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/Hospede.jpg")));
-		BarraLateral.add(lblHospede, "cell 0 5,grow");
-
-		JLabel lblAtividades = new JLabel("Atividades");
-		lblAtividades.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				TelaAtividades TelaAtiv = new TelaAtividades( );
-				TelaAtiv.setVisible(true);
-				TelaAtiv.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-				dispose();
-			}
-		});
-		lblAtividades.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-		lblAtividades.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/Atividades.jpg")));
-		BarraLateral.add(lblAtividades, "cell 0 2,grow");
-
-		JLabel lblQuartos = new JLabel("Quartos");
-		lblQuartos.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				TelaDeAcomodacoes TelaAco = new TelaDeAcomodacoes( );
-				TelaAco.setVisible(true);
-				TelaAco.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-				dispose();
-			}
-		});
-		lblQuartos.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-		lblQuartos.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/Quartos.jpg")));
-		BarraLateral.add(lblQuartos, "cell 0 3,grow");
-
-		JLabel lblServicos = new JLabel("Serviços");
-		lblServicos.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				TelaServicos TelaServ = new TelaServicos( );
-				TelaServ.setVisible(true);
-				TelaServ.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-				dispose();
-			}
-		});
-		lblServicos.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-		lblServicos.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/Servicos.jpg")));
-		BarraLateral.add(lblServicos, "cell 0 4,grow");
-
-		JLabel lblNewLabel_15 = new JLabel("Funcionários");
-		lblNewLabel_15.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				AdminFuncionarios TelaAdm = new AdminFuncionarios( );
-				TelaAdm.setVisible(true);
-				TelaAdm.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-				dispose();
-
-			}
-		});
-		lblNewLabel_15.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/funcionarios.png")));
-		lblNewLabel_15.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-		BarraLateral.add(lblNewLabel_15, "cell 0 6");
-
-		JLabel lblNewLabel_2 = new JLabel("Conta");
-		lblNewLabel_2.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Conta telaConta = new Conta( );
-				telaConta.setVisible(true);
-				telaConta.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-				dispose();
-			}
-		});
-		lblNewLabel_2.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/conta.png")));
-		lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 22));
-		BarraLateral.add(lblNewLabel_2, "cell 0 7");
-
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(255, 255, 255));
-		BarraLateral.add(panel, "cell 0 9,growx,aligny baseline");
-		panel.setLayout(new MigLayout("", "[][]", "[][30.00][29.00][32.00]"));
-
-		JLabel lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/Avatar.jpg")));
-		panel.add(lblNewLabel_4, "cell 0 0 1 3,alignx center");
-
-		JLabel lblNome = new JLabel("Erik Roncaglio");
-		lblNome.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		panel.add(lblNome, "cell 1 1,aligny bottom");
-		lblNome.setText(Func.getNome() + " " + Func.getSobrenome());
-
-		JLabel lblNewLabel_3 = new JLabel(Func.getEmailFunc());
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		panel.add(lblNewLabel_3, "cell 1 2,aligny top");
-
-		JLabel lblNewLabel_5 = new JLabel("Sair");
-		lblNewLabel_5.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Login novoLogin = new Login();
-				novoLogin.setExtendedState(JFrame.MAXIMIZED_BOTH);
-				novoLogin.setVisible(true);
-				dispose();
-
-			}
-		});
-		lblNewLabel_5.setForeground(new Color(0, 0, 0));
-		lblNewLabel_5.setFont(new Font("Times New Roman", Font.PLAIN, 16));
-		lblNewLabel_5.setBackground(new Color(255, 255, 255));
-		lblNewLabel_5.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/Sair.png")));
-		panel.add(lblNewLabel_5, "cell 0 3 2 1,alignx center,aligny top");
-
-		JPanel BarraSuperior = new JPanel();
-		BarraSuperior.setBackground(new Color(255, 255, 255));
-		contentPane.add(BarraSuperior, "cell 0 0 2 1,grow");
-		BarraSuperior.setLayout(new MigLayout("",
-				"[40px:54.00px:40px][150.00][300px:360.00px:300px][grow][40px:40px:40px,right]", "[29.00px]"));
-
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(255, 255, 255));
-		BarraSuperior.add(panel_3, "cell 0 0,grow");
-		panel_3.setLayout(null);
-
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setBounds(10, 0, 33, 31);
-		lblNewLabel.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/logo.png")));
-		panel_3.add(lblNewLabel);
-
-		JPanel panel_4 = new JPanel();
-		panel_4.setBackground(new Color(255, 255, 255));
-		BarraSuperior.add(panel_4, "cell 2 0,grow");
-		panel_4.setLayout(new MigLayout("", "[20px][251.00px]", "[21px]"));
-
-		JLabel lblNewLabel_6 = new JLabel("");
-		panel_4.add(lblNewLabel_6, "cell 0 0,alignx left,aligny top");
-		lblNewLabel_6.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/search.png")));
-
-		txtPesquisa = new JTextField();
-		panel_4.add(txtPesquisa, "cell 1 0,growx,aligny top");
-		txtPesquisa.setBackground(new Color(245, 245, 245));
-		txtPesquisa.setText("Search");
-		txtPesquisa.setColumns(10);
-
-		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(new Color(255, 255, 255));
-		BarraSuperior.add(panel_2, "cell 4 0,grow");
-
-		JLabel lblNewLabel_8 = new JLabel("");
-		panel_2.add(lblNewLabel_8);
-		lblNewLabel_8.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/SinoNotificacao.jpg")));
+		 
 
 		JPanel Principal = new JPanel();
 		Principal.setBackground(new Color(250, 250, 250));
@@ -563,68 +372,7 @@ public class TelaDeQuartos extends JFrame {
 		atualizarJTable(x);
 		Principal.add(scrollPane1, "cell 2 1,grow");
 
-		JPanel BarraInferior = new JPanel();
-		BarraInferior.setBackground(new Color(255, 255, 255));
-		contentPane.add(BarraInferior, "cell 1 2,grow");
-		BarraInferior.setLayout(
-				new MigLayout("", "[][679.00,grow,center][center][center][center][]", "[42.00,grow,center]"));
-
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(255, 255, 255));
-		BarraInferior.add(panel_1, "cell 4 0,grow");
-		panel_1.setLayout(new MigLayout("", "[][][][]", "[]"));
-
-		JLabel lblInstagram = new JLabel("");
-		lblInstagram.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				try {
-					Desktop.getDesktop()
-							.browse(new URL("https://www.instagram.com/stellar_.hotel?igsh=bDl2dmkwY2MzNHFy").toURI());
-				} catch (MalformedURLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (URISyntaxException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
-		panel_1.add(lblInstagram, "cell 0 0");
-		lblInstagram.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/instagram.png")));
-
-		JLabel lblFacebook = new JLabel("");
-		panel_1.add(lblFacebook, "cell 1 0");
-		lblFacebook.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/Facebook.jpg")));
-
-		JLabel lblWhatsapp = new JLabel("");
-		panel_1.add(lblWhatsapp, "cell 2 0");
-		lblWhatsapp.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/Whatsapp.jpg")));
-
-		JLabel lblTwitter = new JLabel("");
-		lblTwitter.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				try {
-					Desktop.getDesktop()
-							.browse(new URL("https://x.com/Stellar1933323?t=sMKnmdFjz2z29kZNNmOY3g&s=09").toURI());
-				} catch (MalformedURLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (URISyntaxException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
-		panel_1.add(lblTwitter, "cell 3 0");
-		lblTwitter.setIcon(new ImageIcon(TelaDeQuartos.class.getResource("/visao/twitter.jpg")));
+	 
 
 		lblNewLabel_16.addMouseListener(new MouseAdapter() {
 			@Override
@@ -696,5 +444,6 @@ public class TelaDeQuartos extends JFrame {
 				}
 			}
 		});
+		setPrincipalPanel(Principal);
 	}
 }
