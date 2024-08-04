ce8993104fc3904bda22d7291100d617ac69aac8
GabrielFelipeMohr <gabriel.f2005@aluno.ifsc.edu.br>
2024-08-04 13:56:37 -0300

    Botão funcionando onde vi que precisava

--- HotelFazenda/src/main/java/visao/Login.java
+++ HotelFazenda/src/main/java/visao/Login.java
@@ -24,13 +24,15 @@ import modelo.Funcionarios;
 import modelo.Usuarios;
 import net.miginfocom.swing.MigLayout;
 import utils.DefaultIconButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtLogin;
-	private JTextField txtSenha;
+	private JPasswordField passwordField;
 	ArrayList<Usuarios> Lista;
 	private JTable table;
 
@@ -72,9 +74,7 @@ public class Login extends JFrame {
 
 		JPanel panel = new JPanel();
 		contentPane.add(panel, "cell 0 0,grow");
-		panel.setLayout(new MigLayout("",
-				"[:84px:84px,grow][84px][210.00px,grow][:125px:125px][][119px][143px,grow][115px]",
-				"[:1.00px:14px][:50px:50px][:125px:125px,grow][14.00px][38.00px][:30px:30px,grow][:70px:70px,grow][33.00px][:30px:30px,grow][93.00,grow][:45px:45px][:45px:45px][grow]"));
+		panel.setLayout(new MigLayout("", "[:84px:84px,grow][84px][210.00px,grow][:125px:125px][119px][143px,grow][115px][]", "[:1.00px:14px][:50px:50px][:125px:125px,grow][14.00px][38.00px][:30px:30px,grow][:70px:70px,grow][33.00px][:30px:30px,grow][93.00,grow][:45px:45px][:45px:45px][grow]"));
 
 		JLabel lblNewLabel = new JLabel("Bem vindo ao ");
 		panel.add(lblNewLabel, "flowx,cell 0 0,growx,aligny top");
@@ -89,27 +89,47 @@ public class Login extends JFrame {
 
 		txtLogin = new JTextField();
 		txtLogin.setBorder(new RoundedBorder(Color.black, 10)); // Altere a cor e o raio conforme necessário
-		panel.add(txtLogin, "cell 0 5 8 1,grow");
+		panel.add(txtLogin, "cell 0 5 7 1,grow");
 		txtLogin.setColumns(10);
 
 		JLabel lblNewLabel_3_1 = new JLabel("Digite sua senha");
 		lblNewLabel_3_1.setFont(new Font("Times New Roman", Font.PLAIN, 24));
 		panel.add(lblNewLabel_3_1, "cell 0 7,growx,aligny top");
 
-		txtSenha = new JPasswordField(20);
-		txtSenha.setBorder(new RoundedBorder(Color.black, 10));
-		txtSenha.setToolTipText("Digite sua senha");
-		txtSenha.setColumns(10);
-		panel.add(txtSenha, "cell 0 8 8 1,grow");
+		passwordField = new JPasswordField(20);
+		passwordField.setBorder(new RoundedBorder(Color.black, 10));
+		passwordField.setToolTipText("Digite sua senha");
+		passwordField.setColumns(10);
+		passwordField.setEchoChar('*');
+		panel.add(passwordField, "cell 0 8 7 1,grow");
 
 		JLabel lblNewLabel_1 = new JLabel("Hotel Fazenda");
 		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 11));
 		lblNewLabel_1.setForeground(new Color(117, 187, 68));
 		panel.add(lblNewLabel_1, "cell 0 0,alignx left,aligny top");
+		
+		ImageIcon Ver = new ImageIcon(Conta.class.getResource("/visao/Ver.png"));
+		ImageIcon NaoVer = new ImageIcon(Conta.class.getResource("/visao/NaoVer.png"));
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (lblNewLabel_4.getIcon() == Ver) {
+					lblNewLabel_4.setIcon(NaoVer);
+					passwordField.setEchoChar((char) 0);
+				} else if (lblNewLabel_4.getIcon() == NaoVer) {
+					lblNewLabel_4.setIcon(Ver);
+					passwordField.setEchoChar('*');
+				}
+			}
+		});
+		lblNewLabel_4.setIcon(Ver);
+		panel.add(lblNewLabel_4, "cell 7 8");
 
 		JLabel lblNewLabel_2 = new JLabel("Esqueceu sua senha?");
 		lblNewLabel_2.setForeground(new Color(0, 128, 255));
-		panel.add(lblNewLabel_2, "cell 7 9,alignx right,aligny top");
+		panel.add(lblNewLabel_2, "cell 6 9,alignx right,aligny top");
 
 		ImageIcon icon = new ImageIcon(getClass().getResource("/visao/Sair.png"));
 		DefaultIconButton btnNewButton = new DefaultIconButton("Entrar", icon);
@@ -118,7 +138,7 @@ public class Login extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 
 				String login = txtLogin.getText();
-				String senha = txtSenha.getText();
+				String senha = passwordField.getText();
 
 				UsuariosDAO dao = UsuariosDAO.getInstancia();
 
@@ -141,7 +161,7 @@ public class Login extends JFrame {
 					TelaErro erro = new TelaErro("Algum Erro Ocorreu");
 					erro.setVisible(true);
 					txtLogin.setBorder(new RoundedBorder(Color.RED, 10)); // Mudando a cor da borda para vermelho
-					txtSenha.setBorder(new RoundedBorder(Color.RED, 10));
+					passwordField.setBorder(new RoundedBorder(Color.RED, 10));
 				}
 
 			}
